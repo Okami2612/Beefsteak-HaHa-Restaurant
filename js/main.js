63a7b53503581e0c8cac8206c50fed421474ec15
Okami2612 <51777169+Okami2612@users.noreply.github.com>
2021-05-18 07:02:49 +0700

    18

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-// Init AOS
-function aos_init() {
-    AOS.init({
-        duration:1000,
-        once:true,
-    });
+// Toggle navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav() {
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
 }
-$(window).on('load', function() {
-    aos_init();
-});
+// Close nav when click nav-item
+document.addEventListener("click", function(e) {
+    if(e.target.closest(".nav-item")){
+        toggleNav();
+    }
+})
+// Sticky header
+window.addEventListener("scroll", function() {
+    if(this.pageYOffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+    }
+    else{
+        document.querySelector(".header").classList.remove("sticky");
+    }
+})
+// Menu tabs
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e) {
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+})
